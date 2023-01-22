47ef1adf57aaa654065e29e75e9ca9e3ac610206
Shai Sachs <ssachs@chewy.com>
2023-01-22 08:38:10 -0500

    basic crud functionality for books
    Signed-off-by: Shai Sachs <ssachs@chewy.com>

---
+++ src/main/java/com/chewy/bookstore/Controllers/BookController.java
@@ -0,0 +1,65 @@
+package com.chewy.bookstore.controllers;
+
+import java.util.HashMap;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.chewy.bookstore.models.Book;
+import com.chewy.bookstore.repositories.BookRepository;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/books")
+    public List <Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity <Book> getBookById(@PathVariable(value = "id") Long bookId) {
+        Book book = bookRepository.findById(bookId).orElse(null);
+        return ResponseEntity.ok().body(book);
+    }
+
+    @PostMapping("/books")
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @PutMapping("/books/{id}")
+    public ResponseEntity <Book> updateBook(@PathVariable(value = "id") Long bookId,
+        @Valid @RequestBody Book bookDetails) {
+        Book book = bookRepository.findById(bookId).orElse(null);
+
+        book.setTitle(bookDetails.getTitle());
+        final Book updatedBook = bookRepository.save(book);
+        return ResponseEntity.ok(updatedBook);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public Map <String, Boolean> deleteBook(@PathVariable(value = "id") Long bookId) {
+        Book book = bookRepository.findById(bookId).orElse(null);
+
+        bookRepository.delete(book);
+        Map < String, Boolean > response = new HashMap < > ();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+}
---
+++ src/main/java/com/chewy/bookstore/Models/Book.java
@@ -0,0 +1,87 @@
+package com.chewy.bookstore.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    private long id;
+    private String title;
+    private String author;
+    private String publisher;
+    private int yearPublished;
+    private int price;
+
+    public Book() {}
+
+    public Book(long id, String title, String author, String publisher, int yearPublished, int price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.yearPublished = yearPublished;
+        this.price = price;
+    }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "title", nullable = false)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "author", nullable = false)
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Column(name = "publisher", nullable = false)
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    @Column(name = "yearPublished", nullable = false)
+    public int yearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    @Column(name = "price", nullable = false)
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
