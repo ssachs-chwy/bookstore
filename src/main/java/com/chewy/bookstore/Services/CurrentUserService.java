4152396d51986cd6c5508cc92debbb84d8021693
Shai Sachs <ssachs@chewy.com>
2023-01-24 21:44:26 -0500

    set user service to request scope
    Signed-off-by: Shai Sachs <ssachs@chewy.com>

--- src/main/java/com/chewy/bookstore/Services/CurrentUserService.java
+++ src/main/java/com/chewy/bookstore/Services/CurrentUserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.annotation.RequestScope;
 
 @Component
+@RequestScope
 public class CurrentUserService {
     private boolean isInStore;
 
